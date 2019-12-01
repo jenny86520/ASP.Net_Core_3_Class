059c45da0342f7e6cbfa050e88d6982e39dddc2b
jenny86520 <jenny50626000@gmail.com>
2019-12-01 16:04:52 +0800

    Class-1201: 練習發佈 ASP․NET Core 應用程式

---
+++ DeployTest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace DeployTest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
