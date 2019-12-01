4a8aae04f59ef037aea03f03d48a45e6fad24be0
jenny86520 <jenny50626000@gmail.com>
2019-12-01 13:43:08 +0800

    Class-1201: 練習 .NET Standard 多目標架構作法

--- solution1/classlib1/Class1.cs
+++ solution1/classlib1/Class1.cs
@@ -4,8 +4,15 @@ namespace classlib1
 {
     public class Class1
     {
-        public string Go(){
-            return "I'm lib!";
+        public string Go()
+        {
+            /** [start] 練習 .NET Standard 多目標架構作法 */
+#if NET472
+            return "net 4.7.2";
+#else
+            return ".net standard 2.0";
+            /** [end] 練習 .NET Standard 多目標架構作法 */
+#endif
         }
     }
 }
