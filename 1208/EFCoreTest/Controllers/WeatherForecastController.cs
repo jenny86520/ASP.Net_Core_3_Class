34fb5bdcd85fca9b10e5a81e93a29bb60098d9fa
jenny86520 <jenny50626000@gmail.com>
2019-12-08 16:23:06 +0800

    Class-1208: 使用 EF Core + ASP․NET Core Web API 針對 Course 表格進行開發

--- 1208/EFCoreTest/Controllers/WeatherForecastController.cs
+++ 1208/EFCoreTest/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿/** Class-1208: 使用 EF Core + ASP․NET Core Web API 針對 Course 表格進行開發 */
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -34,6 +35,18 @@ namespace EFCoreTest.Controllers
                 Summary = Summaries[rng.Next(Summaries.Length)]
             })
             .ToArray();
+        }
+        [HttpGet("{id}")]
+        public WeatherForecast GetFirst(int id)
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray()[id];
         }
     }
 }
