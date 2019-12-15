ba5dc6a8e1cb2539eebc0645498e54867e41dc99
jenny86520 <jenny50626000@gmail.com>
2019-12-15 16:51:51 +0800

    Class-1215: 練習安裝與設定 Serilog 與 Seq 進行記錄管理

--- 1215/SignalRChat/Program.cs
+++ 1215/SignalRChat/Program.cs
@@ -6,21 +6,48 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
 
 namespace SignalRChat
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static int Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            /** Class-1215: 練習安裝與設定 Serilog 與 Seq 進行記錄管理 */
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
+                .Enrich.FromLogContext()
+                .WriteTo.Console()
+                // .WriteTo.Seq("http://localhost:5341") // for Sel
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Starting web host");
+                CreateHostBuilder(args).Build().Run();
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>()
+                    /** Class-1215: 練習安裝與設定 Serilog 與 Seq 進行記錄管理 */
+                    .UseSerilog();
                 });
     }
 }
